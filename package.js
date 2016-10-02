6f0e4f3cc9da25d0ff2fd131ec85dedfa44879b6
matguig <matguig@student.42.fr>
2016-10-02 17:26:10 +0200

    Meteor Package file / package describe

---
+++ package.js
@@ -0,0 +1,7 @@
+Package.describe({
+  name: "matguig:shared-session",
+  version: "0.1.0",
+  summary: "Easily share a session, between the server side and the client side.",
+  git: "https://github.com/matguig/shared-session.git",
+  documentation: "README.md"
+});
