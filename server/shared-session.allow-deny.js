02fa69bd9d32c3f7261af244332925fd49f8ad85
matguig <matguig@student.42.fr>
2016-10-02 17:58:30 +0200

    Mongo collection allow and deny methods

---
+++ server/shared-session.allow-deny.js
@@ -0,0 +1,23 @@
+import { sharedSessionCollection } from "meteor/matguig:shared-session/common/shared-session.js";
+import { sharedSession } from "meteor/matguig:shared-session/server/shared-session.js";
+
+/**
+ * Generic method to allow or not client modification
+ * @param { string } userId (meteor user id)
+ * @param { object } doc
+ * @return { boolean }
+ */
+var allowedGenericMethod = function(userId, doc) {
+    var session = sharedSession.instance();
+
+    if ( doc.session_uid && doc.session_uid !== session._uid ) {
+        return false;
+    }
+    return true;
+};
+
+sharedSessionCollection.allow({
+    insert: allowedGenericMethod,
+    update: allowedGenericMethod,
+    remove: allowedGenericMethod
+});
