590895526ce7015bc7b49e5418b8287a05d7e1da
matguig <matguig@student.42.fr>
2016-10-02 17:57:50 +0200

    Publish meteor collection

--- package.js
+++ package.js
@@ -10,5 +10,8 @@ Package.onUse(function( api ) {
   api.versionsFrom("1.4.1.1");
   api.use(["ecmascript", "matb33:collection-hooks"]);
   api.addFiles("common/shared-session.js");
+  api.addFiles([
+    "server/shared-session.publish.js"
+  ], "server");
   api.mainModule("server/shared-session.js", "server");
 });
---
+++ server/shared-session.publish.js
@@ -0,0 +1,10 @@
+import { collectionName } from "meteor/matguig:shared-session/common/shared-session.js";
+import { sharedSession } from "meteor/matguig:shared-session/server/shared-session.js";
+
+Meteor.publish( collectionName, function ( ) {
+    var uid = this.connection.id;
+    var mySession = sharedSession.instance( uid );
+
+    this.connection.onClose( mySession.clearAll.bind( mySession ) );
+    return mySession.getPublishedCollection( );
+});
