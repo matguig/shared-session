b650d316bed37dc3d5c64bd485a696c62911a39a
matguig <guigon.matthieu@gmail.com>
2016-12-03 14:34:40 +0100

    Change version of this package
    Change version of this package
    to update it on atmospherejs

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "matguig:shared-session",
-  version: "0.1.1",
+  version: "0.2.0",
   summary: "Easily share a session, between the server side and the client side.",
   git: "https://github.com/matguig/shared-session.git",
   documentation: "README.md"
